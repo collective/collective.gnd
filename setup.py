0c77c0b044d43563169474c217dc96be8457642a
MrTango <md@derico.de>
2019-06-20 12:27:27 +0300

    Back to development: 1.0b3

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = '\n\n'.join([
 
 setup(
     name='collective.gnd',
-    version='1.0b2',
+    version='1.0b3.dev0',
     description="Plone addon which provides a GND resolver and a BEACON API.",
     long_description=long_description,
     # Get more from https://pypi.python.org/pypi?%3Aaction=list_classifiers

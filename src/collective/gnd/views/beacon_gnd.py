f05001a3c7daaa083f3a7078a4d2a7b60afff450
Uwe Kretschmer <kretschmer@saw-leipzig.de>
2019-12-06 15:21:16 +0100

    Updated documentation and applied some code styles.

--- src/collective/gnd/views/beacon_gnd.py
+++ src/collective/gnd/views/beacon_gnd.py
@@ -9,6 +9,8 @@ from datetime import datetime
 
 
 class BeaconGnd(BrowserView):
+    """Generates a plain textfile in Beacon format. See
+    https://de.wikipedia.org/wiki/Wikipedia:BEACON/Format for more Details."""
 
     def __call__(self):
         self.portal = api.portal.get()
@@ -22,29 +24,32 @@ class BeaconGnd(BrowserView):
     def get_gnd_ids(self):
         catalog = api.portal.get_tool(name='portal_catalog')
         if self.settings.render_all:
-            # returns all indexed IDs without security check
+            # Returns all indexed IDs without security check
             index = catalog._catalog.getIndex('gnd_id')
             return [id for id in index._index if id]
         else:
-            # returns only those IDs of objects accessable to the user
+            # Returns only those IDs of objects accessable to the user
             brains = catalog(gnd_id={'query': 4, 'range': 'min'},
                              sort_on='gnd_id')
             return [brain.gnd_id for brain in brains]
 
     def gen_gnd_format(self):
-        header_lines = []
-        header_lines.append(u'#FORMAT: BEACON')
-        header_lines.append(u'#PREFIX: http://d-nb.info/gnd/')
-        header_lines.append(u'#LINK: http://www.w3.org/2000/01/rdf-schema#seeAlso')
-        header_lines.append(u'#TARGET: {0}/resolvegnd/{{ID}}'.format(self.portal_url))
-        header_lines.append(u'#MESSAGE: {0}'.format(self.settings.message))
-        header_lines.append(u'#FEED: {0}/beacon-gnd.txt'.format(self.portal_url))
-        header_lines.append(u'#CONTACT: {0}'.format(self.settings.contact))
-        header_lines.append(u'#INSTITUTION: {0}'.format(self.settings.institution))
-        header_lines.append(u'#DESCRIPTION: {0}'.format(self.settings.description))
-        header_lines.append(u'#TIMESTAMP: {0}'.format(datetime.now()))
-        header_lines.append(u'#UPDATE: always')
+        # Build header
+        content_dict = [
+            u'#FORMAT: BEACON',
+            u'#PREFIX: http://d-nb.info/gnd/',
+            u'#LINK: http://www.w3.org/2000/01/rdf-schema#seeAlso',
+            u'#TARGET: {0}/resolvegnd/{{ID}}'.format(self.portal_url),
+            u'#MESSAGE: {0}'.format(self.settings.message),
+            u'#FEED: {0}/beacon-gnd.txt'.format(self.portal_url),
+            u'#CONTACT: {0}'.format(self.settings.contact),
+            u'#INSTITUTION: {0}'.format(self.settings.institution),
+            u'#DESCRIPTION: {0}'.format(self.settings.description),
+            u'#TIMESTAMP: {0}'.format(datetime.now()),
+            u'#UPDATE: always',
+        ]
+        # Get and add GND IDs to content
         gnd_ids = self.get_gnd_ids()
-        header_lines.extend(gnd_ids)
-        header = u'\n'.join(header_lines)
-        return header
+        content_dict.extend(gnd_ids)
+        content = u'\n'.join(content_dict)
+        return content

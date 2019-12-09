d048b28550518602cec6a87367bb66decb627115
Uwe Kretschmer <kretschmer@saw-leipzig.de>
2019-12-09 09:06:26 +0100

    Fixed Flake8 error.

--- src/collective/gnd/behaviors/gnd_id.py
+++ src/collective/gnd/behaviors/gnd_id.py
@@ -3,7 +3,6 @@
 from collective.gnd import _
 from plone import schema
 from plone.autoform.interfaces import IFormFieldProvider
-from plone.dexterity.interfaces import IDexterityContent
 from plone.supermodel import model
 from zope.component import adapter
 from zope.interface import Interface
